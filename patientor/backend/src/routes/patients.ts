a36084017cdd0e1709a40bcb81e3826c9a8ffb5e
Student <student@sirafrica.org>
2026-08-24 11:46:24 +0300

    9.13 Patientor backend, step5

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,21 +1,51 @@
-import express, { type Response } from 'express';
+import express, { type Request, type Response } from 'express';
 import patients from '../../data/patients.ts';
-import type { PatientWithoutSSN } from '../types.ts';
+import type { Patient, PatientWithoutSSN } from '../types.ts';
+import { v1 as uuid } from 'uuid';
 
 const router = express.Router();
 
+type PatientRequestBody = Omit<Patient, 'id'>;
+
+
 router.get('/', (_req, res: Response<PatientWithoutSSN[]>) => {
-  const patientsWithoutSSN: PatientWithoutSSN[] = patients.map(
-    ({ id, name, dateOfBirth, gender, occupation }) => ({
+  res.json(
+    patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
       id,
       name,
       dateOfBirth,
       gender,
       occupation
-    })
+    }))
   );
-
-  res.send(patientsWithoutSSN);
 });
+
+router.post(
+  '/',
+  (req: Request<unknown, PatientWithoutSSN, PatientRequestBody>, res: Response<PatientWithoutSSN>) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+
+  const newPatient = {
+    id: uuid(),
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  };
+
+  patients.push(newPatient);
+
+  const patientWithoutSSN: PatientWithoutSSN = {
+    id: newPatient.id,
+    name: newPatient.name,
+    dateOfBirth: newPatient.dateOfBirth,
+    gender: newPatient.gender,
+    occupation: newPatient.occupation
+  };
+
+  res.json(patientWithoutSSN);
+  }
+);
 
 export default router;
