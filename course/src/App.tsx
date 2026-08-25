9a97d5bb3282ccb38c062d9da5d1f8e371e48f6c
Student <student@sirafrica.org>
2026-08-25 12:05:26 +0300

    9.18 Course, step2

--- course/src/App.tsx
+++ course/src/App.tsx
@@ -1,34 +1,170 @@
-interface Part {
+interface CoursePartBase {
   name: string
   exerciseCount: number
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: 'basic'
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number
+  kind: 'group'
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string
+  kind: 'background'
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[]
+  kind: 'special'
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartSpecial
+
+const courseParts: CoursePart[] = [
+  {
+    name: 'Fundamentals',
+    exerciseCount: 10,
+    description: 'This is an awesome course part',
+    kind: 'basic'
+  },
+  {
+    name: 'Using props to pass data',
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    kind: 'group'
+  },
+  {
+    name: 'Basics of type Narrowing',
+    exerciseCount: 7,
+    description: 'How to go from unknown to string',
+    kind: 'basic'
+  },
+  {
+    name: 'Deeper type usage',
+    exerciseCount: 14,
+    description: 'Confusing description',
+    backgroundMaterial:
+      'https://type-level-typescript.com/template-literal-types',
+    kind: 'background'
+  },
+  {
+    name: 'TypeScript in frontend',
+    exerciseCount: 10,
+    description: 'a hard part',
+    kind: 'basic'
+  },
+  {
+    name: 'Backend development',
+    exerciseCount: 21,
+    description: 'Typing the backend',
+    requirements: ['nodejs', 'jest'],
+    kind: 'special'
+  }
+]
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  )
 }
 
 interface HeaderProps {
   name: string
 }
 
-interface ContentProps {
-  parts: Part[]
-}
-
-interface TotalProps {
-  total: number
-}
-
 const Header = (props: HeaderProps) => {
   return <h1>{props.name}</h1>
+}
+
+interface PartProps {
+  part: CoursePart
+}
+
+const Part = (props: PartProps) => {
+  const part = props.part
+
+  switch (part.kind) {
+    case 'basic':
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>{part.description}</p>
+        </div>
+      )
+
+    case 'group':
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Group projects: {part.groupProjectCount}</p>
+        </div>
+      )
+
+    case 'background':
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>{part.description}</p>
+          <p>
+            Background material:{' '}
+            <a href={part.backgroundMaterial}>
+              {part.backgroundMaterial}
+            </a>
+          </p>
+        </div>
+      )
+
+    case 'special':
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>{part.description}</p>
+          <p>Requirements:</p>
+          <ul>
+            {part.requirements.map(requirement => (
+              <li key={requirement}>{requirement}</li>
+            ))}
+          </ul>
+        </div>
+      )
+
+    default:
+      return assertNever(part)
+  }
+}
+
+interface ContentProps {
+  parts: CoursePart[]
 }
 
 const Content = (props: ContentProps) => {
   return (
     <div>
       {props.parts.map(part => (
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
+        <Part key={part.name} part={part} />
       ))}
     </div>
   )
+}
+
+interface TotalProps {
+  total: number
 }
 
 const Total = (props: TotalProps) => {
@@ -37,21 +173,6 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = 'Half Stack application development'
-
-  const courseParts: Part[] = [
-    {
-      name: 'Fundamentals',
-      exerciseCount: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exerciseCount: 7
-    },
-    {
-      name: 'Deeper type usage',
-      exerciseCount: 14
-    }
-  ]
 
   const totalExercises = courseParts.reduce(
     (sum, part) => sum + part.exerciseCount,
